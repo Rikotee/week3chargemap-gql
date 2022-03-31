977ffed29c065086b4a2400ec09d4793c709a3a2
Rikotee <teerek81@gmail.com>
2022-03-31 11:41:25 +0300

    chargemap gql

---
+++ models/connections.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import connectionTypes from './connectionTypes';
+
+const Schema = mongoose.Schema;
+
+const connectionsSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  ConnectionTypeID: { type: Schema.Types.ObjectId, ref: connectionTypes },
+});
+
+export default mongoose.model('Connections', connectionsSchema);
---
+++ models/stationModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import connections from './connections';
+
+const Schema = mongoose.Schema;
+
+const stationSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  Title: String,
+  Town: String,
+  AddressLine1: String,
+  StateOrProvince: String,
+  Postcode: String,
+  Location: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'], // 'location.type' must be 'Point'
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  },
+  Connections: [{ type: Schema.Types.ObjectId, ref: connections }]
+});
+
+export default mongoose.model('Station', stationSchema);
