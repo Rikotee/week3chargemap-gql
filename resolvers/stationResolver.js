b1be04102e5288c43adefc2721b14294493df8f4
Rikotee <teerek81@gmail.com>
2022-04-01 10:38:27 +0300

    geo

--- resolvers/stationResolver.js
+++ resolvers/stationResolver.js
@@ -1,9 +1,22 @@
 import Station from '../models/station';
+import {rectangleBounds} from '../utils/rectangleBounds';
 
 export default {
   Query: {
     stations: async (parent, args) => {
-      return await Station.find().skip(args.start).limit(args.limit);
+      const mapBounds = rectangleBounds(
+        args.bounds._northEast,
+        args.bounds._southWest
+      );
+      console.log('stResolver', mapBounds);
+      // return await Station.find().skip(args.start).limit(args.limit);
+      return await Station.find({
+        location: {
+          $geoWithin: {
+            $geometry: mapBounds,
+          },
+        },
+      });
     },
   },
 };
