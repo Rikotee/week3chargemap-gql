ab78c73e25076010a7b239c5254a8093740b18eb
Rikotee <teerek81@gmail.com>
2022-04-08 11:35:18 +0300

    security

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,23 +1,19 @@
-import {gql} from 'apollo-server-express';
+import { gql } from 'apollo-server-express';
 
 export default gql`
   extend type Query {
-    user(id: Id!): User
+    user(id: ID!): User
     login(username: String!, password: String!): User
   }
-  
+
   extend type Mutation {
-    registerUser(
-      username: String!,
-      password: String!,
-      full_name: String,
-    ): User
+    registerUser(username: String!, password: String!, full_name: String): User
   }
-  
+
   type User {
     id: ID
-    username: String,
-    full_name: String,
+    username: String
+    full_name: String
     token: String
   }
 `;
